d20bb18d67fd133067f3cd082ba8c6d4ef2b51f7
ljmasdf <ljmasdf@163.com>
2016-05-17 10:22:11 +0800

    c

--- strategies/cutlost.py
+++ strategies/cutlost.py
@@ -36,29 +36,30 @@ class Strategy(StrategyTemplate):
         #         self.user.sell(stock_code = position['stock_code'],price = position['last_price'],amount = position['enable_amount'])
 
     def handle_data(self,context, data):
-        for k,v in data.items():
-            if(v['ask1_volume'] != 0 and v['ask2_volume']  == 0 
-                and v['ask3_volume']  == 0 and v['ask4_volume']  == 0 
-                and v['ask5_volume']  == 0 and not v['name'].startswith('ST')
-                and not v['name'].startswith('*ST') and not k.startswith('300')
-                and v['ask1'] < 50):
-                h=ts.get_hist_data(k).head(2)
-                pc = h.p_change.sum()
-                if(pc < 12):
-                    self.user.buy(stock_code = k,price = v['ask1'],volume = self.user.balance[0]['enable_balance'])
-                print(k)
-                print(v)
-                print(v['ask1'])
-                print(self.user.balance[0]['enable_balance'])
-        #print(data.data)
-        #log.info("cutlost handledata")
+        # for k,v in data.items():
+        #     if(v['ask1_volume'] != 0 and v['ask2_volume']  == 0 
+        #         and v['ask3_volume']  == 0 and v['ask4_volume']  == 0 
+        #         and v['ask5_volume']  == 0 and not v['name'].startswith('ST')
+        #         and not v['name'].startswith('*ST') and not k.startswith('300')
+        #         and v['ask1'] < 50):
+        #         h=ts.get_hist_data(k).head(2)
+        #         pc = h.p_change.sum()
+        #         if(pc < 12):
+        #             self.user.buy(stock_code = k,price = v['ask1'],volume = self.user.balance[0]['enable_balance'])
+        #         print(k)
+        #         print(v)
+        #         print(v['ask1'])
+        #         print(self.user.balance[0]['enable_balance'])
+
         positions = self.user.position
+
+        log.info(self.user.position)
 
         for position in positions:
             if position['market_value'] == 0 or position['enable_amount'] <=0 :
                 continue
             r = (position['income_balance']/position['market_value'])*100
-            if r < -3 and position['enable_amount'] >0:
+            if r < -7 and position['enable_amount'] >0:
                 self.user.sell(stock_code = position['stock_code'],price = position['last_price'],amount = position['enable_amount'])
-            if (data[position['stock_code']]['ask1_volume'] != 0 and position['enable_amount'] > 0):
-                 self.user.sell(stock_code = position['stock_code'],price = position['last_price'],amount = position['enable_amount'])
+            # if (data[position['stock_code']]['ask1_volume'] != 0 and position['enable_amount'] > 0):
+            #      self.user.sell(stock_code = position['stock_code'],price = position['last_price'],amount = position['enable_amount'])
